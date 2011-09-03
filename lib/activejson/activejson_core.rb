d44dff28f77d74efbaa84a4e76475e2847151a19
Andy Caldwell <andy.m.caldwell@googlemail.com>
2011-09-03 18:24:56 +0100

    Make arrays more explicit and auto-walk them

--- lib/activejson/activejson_core.rb
+++ lib/activejson/activejson_core.rb
@@ -6,25 +6,37 @@ MultiJson.engine = :yajl
 module ActiveJson
   class Core
     instance_methods.each {|m| undef_method(m) unless %w(__id__ __send__ to_json instance_eval nil? is_a? class).include?(m.to_s)}
+    attr_accessor :hash
 
-    def initialize(&blk)
+    def initialize()
       @hash = {}
-      blk.call(self)
+      yield self if block_given?
     end
 
     def method_missing(key, *values)
-      value = if block_given?
-        ActiveJson::Core.new do |a| yield a end
-      else
-        values.size == 1 ? values.first : values
-      end
+      value = 
+        if block_given?
+          if values.size == 1
+            values.first.map do |v|
+              new_json_ele = ActiveJson::Core.new
+              yield new_json_ele, v
+              new_json_ele
+            end
+          else
+            new_json_ele = ActiveJson::Core.new
+            yield new_json_ele
+            new_json_ele
+          end
+        else
+          values.size == 1 ? values.first : values
+        end
 
       if @hash[key].nil?
         @hash[key] = value
       elsif @hash[key].is_a? Array
         @hash[key] << value
       else
-        @hash[key] = [@hash[key], value]
+        @hash[key] = value
       end
     end
 

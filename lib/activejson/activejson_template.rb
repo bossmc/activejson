e593ebd0f6ee9a6d145e6e63b921fb2564cd01ea
Andy <andy.m.caldwell@googlemail.com>
2012-08-28 22:01:28 +0100

    Fix to allow activejson to load with rails 3.2+

--- lib/activejson/activejson_template.rb
+++ lib/activejson/activejson_template.rb
@@ -2,13 +2,18 @@ require 'activejson/activejson_core'
 
 module ActionView
   module Template::Handlers
-    class ActiveJsonTemplate < Template::Handler
-      include Compilable
+    class ActiveJsonTemplate
+      class_attribute :default_format
+      self.default_format = 'text/json'
 
       def compile(template) 
         %{ActiveJson::Core.new do |json|
           #{template.source}
         end.to_json }
+      end
+
+      def self.call(template)
+        new.compile(template)
       end
     end
   end
